396d00104907ee502f927df86ed4d7058aa82a5e
George Ferreira <gehwallace@gmail.com>
2022-07-30 14:51:48 -0300

    Pushing innitial code

---
+++ src/main/java/com/api/privateparkingcontrol/PrivateParkingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.privateparkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateParkingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/privateparkingcontrol/PrivateParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.privateparkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
