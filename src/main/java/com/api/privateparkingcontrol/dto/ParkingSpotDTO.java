2483853f5d390d615a16232c3ef72477f5ea0f8c
George Ferreira <gehwallace@gmail.com>
2022-07-30 19:01:26 -0300

    Criando a classe DTO

---
+++ src/main/java/com/api/privateparkingcontrol/dto/ParkingSpotDTO.java
@@ -0,0 +1,37 @@
+package com.api.privateparkingcontrol.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class ParkingSpotDTO {
+
+    @NotBlank
+    private String parkingSpotNumber;
+
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+
+    @NotBlank
+    private String brandCar;
+
+    @NotBlank
+    private String modelCar;
+
+    @NotBlank
+    private String colorCar;
+
+    @NotBlank
+    private String responsibleName;
+
+    @NotBlank
+    private String apartment;
+
+    @NotBlank
+    private String block;
+}
