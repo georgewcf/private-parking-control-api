06f3d8f1a47682346395a911c2a762d17c99abd6
George Ferreira <gehwallace@gmail.com>
2022-07-30 17:25:05 -0300

    Criando a interface repository

---
+++ src/main/java/com/api/privateparkingcontrol/repository/ParkingSpotRepository.java
@@ -0,0 +1,12 @@
+package com.api.privateparkingcontrol.repository;
+
+import com.api.privateparkingcontrol.model.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+}
