a0ce7d1e7f05d94f2e3e151ad17f4afdb04b8ebd
George Ferreira <gehwallace@gmail.com>
2022-07-30 19:10:07 -0300

    Criando a service

--- src/main/java/com/api/privateparkingcontrol/service/ParkingSpotService.java
+++ src/main/java/com/api/privateparkingcontrol/service/ParkingSpotService.java
@@ -1,7 +1,45 @@
 package com.api.privateparkingcontrol.service;
 
+import com.api.privateparkingcontrol.model.ParkingSpotModel;
+import com.api.privateparkingcontrol.repository.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByBlockAndApartment(String block, String apartment) {
+        return parkingSpotRepository.existsByBlockAndApartment(block, apartment);
+    }
+
+    public List<ParkingSpotModel> findAll() {
+        return parkingSpotRepository.findAll();
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
 }
