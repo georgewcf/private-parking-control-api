26ab03b3d5e67b2d7f3e6b7cf8e76004da3e5eec
George Ferreira <gehwallace@gmail.com>
2022-07-30 17:17:55 -0300

    Criando a model e definindo os campos do banco

---
+++ src/main/java/com/api/privateparkingcontrol/model/ParkingSpotModel.java
@@ -0,0 +1,48 @@
+package com.api.privateparkingcontrol.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+
+    @Column(nullable = false, unique = true,length = 7)
+    private String licensePlateCar;
+
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+
+    @Column(nullable = false, length = 70)
+    private LocalDateTime registrationDate;
+
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+
+    @Column(nullable = false, length = 30)
+    private String apartment;
+
+    @Column(nullable = false, length = 30)
+    private String block;
+}
